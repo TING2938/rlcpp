ea3fc2b29a34329a06e6d4084b433f7c99a051f2
yeting <ye.ting@h3c.com>
2022-05-31 14:00:04 +0800

    change localTime()

--- tools/utility.hpp
+++ tools/utility.hpp
@@ -20,12 +20,15 @@ namespace rlcpp
  * @param fmt 时间日期格式，参见 https://en.cppreference.com/w/cpp/io/manip/put_time
  * @return 时间日期字符串
  */
-inline std::string localTime(const char* fmt = "%Y-%m-%d %H:%M:%S %A")
+inline std::string localTime(const char* fmt = "%Y-%m-%d %H:%M:%S %A", std::time_t* t = nullptr)
 {
-    time_t t;
+    std::time_t t_tmp;
     char buf[500];
-    t = time(NULL);
-    strftime(buf, 500, fmt, localtime(&t));
+    if (t == nullptr) {
+        t_tmp = time(NULL);
+        t     = &t_tmp;
+    }
+    strftime(buf, 500, fmt, localtime(t));
     return buf;
 }
 
